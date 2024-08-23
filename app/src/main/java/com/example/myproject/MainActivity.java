063b2d8fc8a36a63d2e0d476e573bc0caf716266
MariaVictoria <123131515+MariaVictoria@users.noreply.github.com>
2024-08-23 13:12:57 -0300

    up

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void presiono(View v) {
+        Toast.makeText(this, "Presioné el botón", Toast.LENGTH_LONG).show();
+    }
+
+    public void presiono2(View v) {
+        Toast.makeText(this, "Presioné el botón 2", Toast.LENGTH_LONG).show();
+    }
+
+    private EditText et1,et2;
+    private TextView tv1;
+
+
+}
